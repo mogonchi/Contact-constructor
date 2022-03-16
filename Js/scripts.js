6a401c26e518de67a7c353d9cbdfeeaaa9a7f6d3
mogonchi <monyenyedolvine@gmail.com>
2022-03-16 21:22:40 +0300

    add js/scripts.js rule

--- Js/scripts.js
+++ Js/scripts.js
@@ -17,3 +17,9 @@ $(document).ready(function() {
 
     });
 })
+$(".contact").last().click(function() {
+    $('#show-contact').show();
+    $('#show-contact h2').text(newContact.firstName);
+    $('#.first-name').text(newContact.firstName);
+    $('.last-name').text(newContact.lastName);
+});
