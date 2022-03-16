659c2be7e8143e4fd9ed19f82e181a7a9ee339ee
mogonchi <monyenyedolvine@gmail.com>
2022-03-16 20:59:10 +0300

    add js/scripts.js

--- Js/scripts.js
+++ Js/scripts.js
@@ -1,4 +1,19 @@
+// business logic
 function Contact(first, last) {
     this.firstName = first;
     this.lastName = last;
 }
+
+// user interface logic
+$(document).ready(function() {
+    $('form#new-contact').submit(function(event) {
+        event.preventDefault();
+        let inputtedFirstName = $('input#new-first-name').val();
+        let inputtedLastName = $('input#new-last-name').val();
+
+        $('ul#contacts').append('<li><span class='contact'>' + newContact.firstname + '</span></li>');
+        $('input#new-first-name').val('');
+        $('input#new-last-name').val('');
+
+    });
+});
